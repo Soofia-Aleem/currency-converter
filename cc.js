57d84b5514fa16227efba8858c3735a542238ff7
Soofia-Aleem <168825688+Soofia-Aleem@users.noreply.github.com>
2024-05-31 00:01:32 +0500

    first commit

---
+++ cc.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+const currency = {
+    PKR: 1, //based currency
+    EUR: 0.0033,
+    GBP: 0.0028,
+    INR: 0.30,
+    USD: 0.0036,
+    IDR: 0.017,
+    KWD: 0.001,
+    ZND: 0.005,
+    LKR: 1.08,
+    XOF: 2.17,
+    IRR: 151.60,
+    COP: 13.90,
+    CNY: 0.026,
+    AFN: 0.25,
+    AMD: 1.39
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "inter from currency",
+        type: "list",
+        choices: ["PKR", "EUR", "GBP", "INR", "USD", "IDR", "KWD", "ZND", "LKR", "XOF", "IRR", "COP", "CNY", "AFN", "AMD"]
+    },
+    {
+        name: "to",
+        message: "inter to currency",
+        type: "list",
+        choices: ["PKR", "EUR", "GBP", "INR", "USD", "IDR", "KWD", "ZND", "LKR", "XOF", "IRR", "COP", "CNY", "AFN", "AMD"]
+    },
+    {
+        name: "amount",
+        message: "inter your amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(Math.round(convertedAmount));
